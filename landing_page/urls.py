7b09dce387b84d503c4cfec9eceeae9cd1411e72
sangeeta-g-d <sangeetadani599@gmail.com>
2024-02-23 15:09:50 +0530

    admin db

--- landing_page/urls.py
+++ landing_page/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     
     path('', views.index, name='index'),
     path('admin_login', views.admin_login, name='admin_login'),
+    path('admin_db', views.admin_db, name='admin_db')
 
 ]
--- landing_page/views.py
+++ landing_page/views.py
@@ -9,4 +9,23 @@ def index(request):
     return render(request,'index.html')
 
 def admin_login(request):
+    if request.method == 'POST':
+        print("hiiiiiiiiiiii")
+        username = request.POST.get('username')
+        print(username)
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None and user.is_superuser:
+            print("yessssss")
+            login(request, user)
+            print("useaaaaaaaaa",user)
+            print(request.user)
+            return redirect('/admin_db')
+        else:
+            
+            return redirect('/admin_login')
     return render(request,'admin_login.html')
+
+
+def admin_db(request):
+    return render(request,'admin_db.html')
