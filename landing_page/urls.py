9cd54ed6fffee1068020789409202ab150d12ee4
vidyawandakar <vidyawandakar@gmail.com>
2024-02-28 15:41:50 +0530

    azsxdfcgv

--- landing_page/urls.py
+++ landing_page/urls.py
@@ -9,9 +9,9 @@ urlpatterns = [
     path('admin_login', views.admin_login, name='admin_login'),
     path('admin_db', views.admin_db, name='admin_db'),
     path('admin_logout', views.admin_logout, name='admin_logout'),
-    path('upload/', views.upload_file, name='upload_file'),
-    path('team/<int:id>', views.team, name='team'),
-    path('resume/<int:id>/<int:u_id>', views.resume, name='resume'),
+    #path('upload/', views.upload_file, name='upload_file'),
+    path('team', views.team, name='team'),
+    path('resume/<int:id>', views.resume, name='resume'),
     path('generate_content', views.generate_content, name='generate_content'),
    
 
